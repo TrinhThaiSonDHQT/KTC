4372f631e6608e4325569bb9128d855d3c7efc76
Trinh Thai Son <ttsondhqt@gmail.com>
2025-07-18 03:09:46 +0700

    update

--- Week-03/Thu/auth-with-reactjs/src/App.tsx
+++ Week-03/Thu/auth-with-reactjs/src/App.tsx
@@ -1,6 +1,7 @@
 import Dashboard from './components/Dashboard';
 import LoginForm from './components/LoginForm';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import NoPage from './components/NoPage';
 
 const App = () => {
   return (
@@ -9,10 +10,11 @@ const App = () => {
         <Route index element={<Dashboard />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/login" element={<LoginForm />} />
+        <Route path="*" element={<NoPage />} />
         {/* <Route path="/tasks" element={<Tasks />} />
              <Route path="/customer" element={<Customer />} />
              <Route path="/access-denied" element={<AccessDenied />} />
-            <Route path="*" element={<AccessDenied />} /> */}
+             */}
       </Routes>
     </BrowserRouter>
   );
